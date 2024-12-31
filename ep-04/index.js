7c33a76e0ad5aae2df45c14c253fb2d64a9f3d6b
Sonu98980 <yoursatyabhai2323@gmail.com>
2024-12-30 22:11:04 -1100

    first commit

---
+++ ep-04/index.js
@@ -0,0 +1,30 @@
+// FUNCTIONS  TASK THAT WE WANT TO EXECUTE MANY TIME
+
+// FUNCTION DECLARATION
+
+// function sum (a,b){
+//     console.log(a+b);  NORMAL FUNCTION
+
+// };
+// sum(3,4)
+
+// FUNCTION EXPRESSION KISI VARIABLES ME ASSIGN KARTE HAI
+
+// a function without name is called anonymous function
+
+// const SumOfTwonumbers = function (num1,num2) {
+// console.log(num1+num2);
+
+
+// };
+// SumOfTwonumbers(1,8);
+
+// const SubsTraction = function (a,b){
+ 
+//  console.log(a>b ? a-b : b-a);
+ 
+// }
+// SubsTraction(4,7);
+
+
+ 
