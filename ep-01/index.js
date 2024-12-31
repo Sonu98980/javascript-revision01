c59ac89e8f92a4a7036e085dcd9ead75d47c2aba
Sonu98980 <yoursatyabhai2323@gmail.com>
2024-12-30 15:02:50 -1100

    first commit

---
+++ ep-01/index.js
@@ -0,0 +1,19 @@
+// TEMPLATE LITERALS
+const age = 40;
+const fullNAME = "sonu"
+
+// "hello mr sonu your age is: 40"
+
+// console.log("hello mr"+fullNAME+"your age is:"+age)
+
+console.log(`hello mr ${fullNAME} your age is: ${age}`);
+
+//  Q PRINT SUM OF TWO NUMBERS 
+
+const a = 20;
+const b = 30;
+
+console.log(`sum of ${a} and ${b} is = ${a+b}`);
+
+
+
