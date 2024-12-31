d647fedff36c8a1daf9f05d521b78e1253b26b0a
Sonu98980 <yoursatyabhai2323@gmail.com>
2024-12-31 00:00:25 -1100

    first commit

---
+++ ep-06/index.js
@@ -0,0 +1,70 @@
+// ARRAYS  ( map , filter , reduce)
+
+// const arr = [30,40,50,"sonu", "male"];
+// // console.log(arr);
+
+// // arr.push("orange")
+// // arr.unshift("apple")
+// arr.push("hero");
+
+// console.log(arr.length)
+
+
+//MAP YE MODIFIED ARRAY RETURN KARTA HAI 
+
+// const arr = [10,20,30];
+// const newArray = arr.map(function(element){
+//     return element*2
+// })
+
+// console.log(newArray)
+
+// const product = [{
+//     price : 1000,
+//     name : "earbuds"},
+
+//     {
+//         price : 3000,
+//         name : "iphone",
+//     },
+    
+//     {
+//         price : 500,
+//         name : "gloves",
+//     },
+// ];
+
+
+
+
+// product.map((element) => {
+//     console.log(element.name)
+// })
+
+
+//FILTER 
+
+// const arr = [20,30,40];
+// const result = arr.filter((element) => {
+//     return element >= 30;
+// })
+// console.log(result);
+
+// const array = [1,2,3,4,5];
+// const result = array.filter((element) => {
+//     return element >= 3;
+
+// })
+// console.log(result)
+
+
+// REDUCE 
+
+// const arr = [2,3,4,5,6];
+// // const ans = arr.reduce((accumulator,element) => {
+// //     return accumulator + element 
+// // })
+// console.log(ans);
+
+
+
