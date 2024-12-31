10089942e3d92a683c404f0e4f17fc558aede341
Sonu98980 <yoursatyabhai2323@gmail.com>
2024-12-30 23:13:23 -1100

    first commit

---
+++ ep-05/index.js
@@ -0,0 +1,97 @@
+// CALLBACKS FUNCTIONS
+
+// const doSomthing = function(callback){
+//     console.log("samose ban rahe hai");
+//     setTimeout(function(){
+//         callback();
+//     },3000)
+// };
+
+// const callback = function (){
+//     console.log("samose ready hai")
+ 
+// };
+// doSomthing(callback)
+
+// const addTION = function (){
+//     let a = 2;
+//     let b = 4;
+//     console.log(a+b);
+//     setTimeout(function(){
+//         callback();
+
+//     },3000);         // output 6 than after 3 sec  output will be 2
+// };
+
+// const callback = function(){
+//     let c = 4;
+//     let d = 6;
+//     console.log(c>d ? c-d : d-c);
+// };
+// const res = {addTION,callback}
+// addTION(callback)
+
+
+// HIGH ORDER FUNCTION 
+
+// const radius = [4,6,7,8];
+// const forMulaOfArea = function (r){
+//     return Math.PI *r*r;
+// };
+
+// const calculate = function (radius,formula){
+//     const result = [];
+//     for (let i = 0; i < radius.length; i++) {
+//       const area = formula(radius[i]);
+//         result.push(area)
+//     }
+// return result
+// }
+
+// const ans = calculate(radius,forMulaOfArea)
+// console.log(ans);
+
+// const radius = [1,2,3,4];
+// const forMulaOfcircumference = function(r){
+// return 2*r
+// };
+
+// const calculate = function (radius,formula){
+//     const result = [];
+//     for (let i = 0; i < radius.length; i++) {
+//       const circumference = formula(radius[i]);
+//         result.push(circumference)
+//     }
+// return result }
+
+// const ans = calculate(radius,forMulaOfcircumference);
+// console.log(ans)
+
+
+
+
+// arrow function ARROW =()=>{}
+
+//     const multiply = (a,b) => {
+// return a*b;
+//     };
+
+//     console.log(multiply(3,5))
+
+
+// const divide = (a,b) => {
+//     return a/b;
+// };
+// console.log(divide(42,6));
+
+// const add = (a,b) => {
+// return a+b;
+// }
+// console.log(add(4,5))
+
+// const subs = (a,b) => {
+//     return a-b;
+    
+// };
+// console.log(subs,(4,7));
+
