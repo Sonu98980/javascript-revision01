6de9bc92b73cacba5de9aaa0ba1db64ba9657917
Sonu98980 <yoursatyabhai2323@gmail.com>
2024-12-31 00:40:31 -1100

    first commit

---
+++ ep-07/index.js
@@ -0,0 +1,59 @@
+// ASYNCHORONOUS  JAVASCRIPT
+
+// JS CAN DO ONE TASK AT A TIME 
+// JS IS A SYNCHONOUS CODE
+
+// GITHUB
+//FETCH API 
+
+// const gitHub_API =" https://api.github.com/users/Sonu98980";
+
+// const fetchdata = async () => {
+//     const result = await fetch(gitHub_API);
+//     const jsonData = await  result.json();
+//     console.log(jsonData)
+// };
+
+// fetchdata()
+
+
+// ERROR HANDLING 
+
+
+// const gitHub_API ="https://api.github.com/users/Sonu98980";
+// const fetchdata = async () => {
+// try {
+//         const result = await fetch(gitHub_API);
+//         const jsonData = await  result.json();
+//         console.log(jsonData)
+//     }
+//  catch(error){
+// console.log(error);
+// }};
+
+// fetchdata();
+
+
+
+// throw error yani khud ka error 
+
+// const myERROR = new Error ("this is my error");
+// // console.log (myERROR);
+// throw new Error("something went wrong")
+
+
+//STORAGE 
+
+// 1. LOCAL STORAGE ( BROWSER PROVIDE KARTA HAI )
+// 2. SESSION STORAGE 
+// 3 COOKIES (BACKEND)
+
+// LOCAL STORAGE > WINDOWS YA WEBSITE OFF HONE PER BHI DATA DELETE NHI HOTA HAI 
+
+// SESSION STORAGE ME OPPOSITE
+
+// LOCALSTORAGE 
+
+// localStorage.setItem("name", "sonu"); name=key and sonu = value
+// console.log(localStorage.getItem("name")); // getitem access krne k lie data ko 
+// localStorage.clear()
